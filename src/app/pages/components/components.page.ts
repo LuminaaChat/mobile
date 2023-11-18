2ed8da7eb6a3fb84cd3e47ec15a81b9ed840d1b4
jaspers <jaspers@adesso-mobile.de>
2023-11-18 16:26:56 +0100

    Add test component, begin styling work

--- src/app/pages/components/components.page.ts
+++ src/app/pages/components/components.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/base-components/button/button.component';
 import { InputFieldComponent } from '../../components/base-components/input-field/input-field.component';
 import { ChatMessageListItemComponent } from '../../components/chat-message-list-item/chat-message-list-item.component';
+import { ListItemComponent } from '../../components/list-item/list-item.component';
 
 @Component({
   templateUrl: './components.page.html',
   styleUrls: ['./components.page.scss'],
-  imports: [ButtonComponent, InputFieldComponent, ChatMessageListItemComponent],
+  imports: [ButtonComponent, InputFieldComponent, ChatMessageListItemComponent, ListItemComponent],
   standalone: true,
 })
 export class ComponentPage {
-  constructor() {}
+  constructor() { }
 }
