d94b5eedbb716c279eecf193881f5bc22b3ee3c9
Christopher Loer <>
2023-11-18 10:19:52 +0100

    update chat list entity

--- src/app/shared/entity/chat/chat-list.entity.ts
+++ src/app/shared/entity/chat/chat-list.entity.ts
@@ -1,1 +1,8 @@
-export class ChatListEntity {}
+import { ChatEntity } from './chat.entity';
+
+export class ChatListEntity {
+  readonly chats: ChatEntity[];
+  constructor(entity: ChatListEntity) {
+    this.chats = entity.chats;
+  }
+}
