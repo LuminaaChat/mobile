e0d2430881867546da10880a4d3e19aece6a2a8a
Christopher Loer <>
2023-11-18 10:19:46 +0100

    update socket url

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { Socket, io } from "socket.io-client";
+import { Socket, io } from 'socket.io-client';
 
 @Injectable({ providedIn: 'root' })
 export class ChatService {
   newChat$ = new Subject<string>();
   private socket: Socket;
   constructor() {
-    this.socket = io('http://10.38.8.77:3000/', { autoConnect: false });
+    this.socket = io('http://localhost:3000/', { autoConnect: false });
   }
 
   connectToSocket(username: string) {
@@ -18,18 +18,18 @@ export class ChatService {
 
   sendMessage(msg: string) {
     if (this.socket.connected) {
-      this.socket.emit("hello", msg);
+      this.socket.emit('hello', msg);
     } else {
-      console.log("no socket found");
+      console.log('no socket found');
     }
   }
 
   private subToEvents() {
-    this.socket.on("connect", () => {
+    this.socket.on('connect', () => {
       console.log('connected:', this.socket.id);
     });
-    this.socket.on("chat", (getMsg: string) => {
-      console.log("chat", getMsg);
+    this.socket.on('chat', (getMsg: string) => {
+      console.log('chat', getMsg);
       this.newChat$.next(getMsg);
     });
   }
